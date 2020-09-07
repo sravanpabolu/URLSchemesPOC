62191512e34e9c858a2062ee4f0454c4cc99dcdd
Sravan <sravankumar.pabolu@cognizant.com>
2020-09-07 10:20:48 +0530

    Initial Commit

---
+++ URLSchemesPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLSchemesPOC
+//
+//  Created by Pabolu, Sravan Kumar (Cognizant) on 07/09/20.
+//  Copyright © 2020 Sravan Kumar Pabolu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
